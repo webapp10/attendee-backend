818d27c5552ce7d4bf4c8f3ef0ee6bf4d0a4190d
Subhranshu Choudhury <testsubhranshu00@gmail.com>
2023-05-25 00:55:28 +0530

    ver 1.0
    - x-access-token -> Cookie

--- app/routes/result.routes.js
+++ app/routes/result.routes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "Cookie, Origin, Content-Type, Accept"
     );
     next();
   });
